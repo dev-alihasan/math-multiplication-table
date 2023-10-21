add84c15cf0a9f7a50e1487687c1e306470682f3
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-10-21 18:05:45 +0600

    update js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-let number = prompt( " If you'd like to know a specific multiplication table, just let me know which one you're interested in, and I'll be happy to help.")
+let number = prompt(
+  " If you'd like to know a specific multiplication table, just let me know which one you're interested in, and I'll be happy to help."
+);
 
-for ( input = 1; input <= 10; input )
+function mlTable(number) {
+  for (input = 1; input <= 10; input++) {
+    document.write(`$ {number} x $ {input} = $ {number} * $ {input} `);
+  }
+}
