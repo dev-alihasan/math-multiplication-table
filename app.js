c3e4b6d681bc41e9e90dd62aa2f1dd0eda391632
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-10-21 18:01:09 +0600

    adding js

---
+++ app.js
@@ -0,0 +1,3 @@
+let number = prompt( " If you'd like to know a specific multiplication table, just let me know which one you're interested in, and I'll be happy to help.")
+
+for ( input = 1; input <= 10; input )
